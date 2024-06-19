ee4861a03dbdb1d866ebeb85acae1270a4b77ab9
Seonghun <77530278+kawai10@users.noreply.github.com>
2024-06-19 14:33:05 +0900

    Add - user 테이블 생성

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    name = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return self.email
