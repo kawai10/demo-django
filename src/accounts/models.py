dfb2f9d1155f3ae52699da9d877769690bd3b90d
Seonghun <77530278+kawai10@users.noreply.github.com>
2024-06-19 16:22:26 +0900

    Add - User Model verbose_name 설정

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -2,14 +2,16 @@ from django.db import models
 
 
 class User(models.Model):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, verbose_name="이메일")
     password = models.CharField(max_length=128)
-    name = models.CharField(max_length=15)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=15, verbose_name="이름")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일")
 
     class Meta:
         db_table = "users"
+        ordering = ["id"]
+        verbose_name_plural = "계정"
 
     def __str__(self):
-        return self.email
+        return self.name
